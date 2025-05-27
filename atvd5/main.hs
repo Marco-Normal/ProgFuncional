c9698b7a03a0d6051bc01478c163a0a72e494dde
LucasLombardiCastro <lucaslombardicastro@gmail.com>
2025-05-27 15:37:26 -0300

    Reading data from CSV, creating list, question 1

--- atvd5/main.hs
+++ atvd5/main.hs
@@ -1,26 +1,45 @@
-import Data.List.Split
-import Data.Map
+import qualified Text.CSV as CSV
+import Text.Read (readMaybe) --Will help us reading the CSV File and Parsing to int the strings
+import Data.Maybe
 
+
+data PandemicData = PandemicData { --Declaring the object
+    country :: [Char],
+    confirmed :: Integer,
+    deaths :: Integer,
+    recovery :: Integer,
+    active :: Integer
+    } deriving (Show, Read, Eq)
+
+--Taking a Record (list of strings in a CSV) and returning maybe a object pandemicData
+parseRecord :: CSV.Record -> Maybe PandemicData 
+parseRecord [country, confirmedStr, deathsStr, recoveryStr, activeStr] = do --We still need to convert the necessary Strings to Int
+     --Converting the necessaries String to String with readMaybe (to handle problematic cases)
+    confirmed <- readMaybe confirmedStr
+    deaths <- readMaybe deathsStr
+    recovery <- readMaybe recoveryStr
+    active <- readMaybe activeStr
+
+    Just $ PandemicData country confirmed deaths recovery active --Constructing a pandemicData if everything was right
+parseRecord _ = Nothing --If one maybe returned nothing, the parseRecord will return nothing 
+
+main :: IO()
 main = do
-    print "Hello Haskell"
+    la <- getLine --Gettint all the line (Impute Function)
+    let n1 = read la --Read is converting to a number (Pure function)
+    
+    --lb <- getLine     
+    --let n2 = read lb
+    --lc <- getLine 
+    --let n3 = read lc 
+    --ld <- getLine       
+    --let n4 = read ld
 
-data Paises = Paises
-    { nome :: [Char]
-    , confirmed :: Integer
-    , deaths :: Integer
-    , recovery :: Integer
-    , active :: Integer
-    }
-    deriving (Show, Eq)
-csvSplit :: [Char] -> Paises
-csvSplit s =
-    case splitOn "," s of
-        [n, c, d, r, a] ->
-            Paises
-                { nome = n
-                , confirmed = read c
-                , deaths = read d
-                , recovery = read r
-                , active = read a
-                }
-	_ -> error "Impossivel"
+    --Reading and saving the records
+    Right csv <- CSV.parseCSVFromFile "dados.csv"
+    let pandemicData = [p | Just p <- map parseRecord csv]  --For each row in CSV, applie parseRecord. Than, include in a list comprehension
+    --First Answer
+    putStrLn $ show $ sum $ map active $ filter ((>n1).confirmed) pandemicData
+    --Second Answer
+    --putStrLn $ show $ sum $ map deaths $ take 3 $ sortBy $ filter ((>8724).confirmed) pandemicData
+    --Third Answer
